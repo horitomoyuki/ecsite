6a0aaa3d414e7b131d151b9652f31d49b946f346
horitomoyuki <eziltom0131@gmail.com>
2021-09-19 15:49:04 +0900

    create-buy_function

--- ecsite/resources/views/cartitem/index.blade.php
+++ ecsite/resources/views/cartitem/index.blade.php
@@ -19,35 +19,37 @@
                 {{ $cartitem->amount }}円
               </div>
               <div class="form-inline">
-                  <!-- 数量を更新するフォームに変更 -->
-                  <form method="POST" action="/cartitem/{{ $cartitem->id }}">
-                      @method('PUT')
-                      @csrf
-                      <input type="text" class="form-control" name="quantity" value="{{ $cartitem->quantity }}">
-                      個
-                      <button type="submit" class="btn btn-primary">更新</button>
-                  </form>
-                  <!-- 削除フォームを追加 -->
-                  <form method="POST" action="/cartitem/{{ $cartitem->id }}">
-                      @method('DELETE')
-                      @csrf
-                      <button type="submit" class="btn btn-primary ml-1">カートから削除する</button>
-                  </form>
-              </div>
-              <div>
-                {{ $cartitem->quantity }}個
+                <form method="POST" action="/cartitem/{{ $cartitem->id }}">
+                    @method('PUT')
+                    @csrf
+                    <input type="text" class="form-control" name="quantity" value="{{ $cartitem->quantity }}">
+                    個
+                    <button type="submit" class="btn btn-primary">更新</button>
+                </form>
+                <form method="POST" action="/cartitem/{{ $cartitem->id }}">
+                    @method('DELETE')
+                    @csrf
+                    <button type="submit" class="btn btn-primary ml-1">カートから削除する</button>
+                </form>
               </div>
             </div>
           @endforeach
         </div>
-    </div>
-    <div class="col-md-4">
+      </div>
+      <div class="col-md-4">
         <div class="card">
           <div class="card-header">
-              小計
+            小計
           </div>
           <div class="card-body">
+            <div>
               {{ $subtotal }}円
+            </div>
+            <div>
+              <a class="btn btn-primary" href="/buy" role="button">
+                レジに進む
+              </a>
+            </div>
           </div>
         </div>
       </div>
